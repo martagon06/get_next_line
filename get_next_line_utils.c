bfbbcb9fde3aa2432d9d74da9a76cac5a0d61e69
martagon06 <miguelmorenomartagon@gmail.com>
2025-03-17 18:25:18 +0100

    .

---
+++ get_next_line_utils.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miguelmo <miguelmo@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 18:48:42 by miguelmo          #+#    #+#             */
+/*   Updated: 2025/03/17 18:24:57 by miguelmo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+ssize_t ft_strlen(const char *s)
+{
+    size_t len;
+
+    if(!s)
+        return (0);
+    len = 0;
+    while (s[len])
+        len++;
+    return (len);
+}
+
+void    set_zero(char *str, ssize_t len)
+{
+    ssize_t cont;
+
+    if (!str)
+        return;
+    cont = 0;
+    while (cont < len)
+    {
+        str[cont] = 0;
+        cont++;
+    }
+}
+
+void    reset_str_sta(char *str_sta, int pos_nl)
+{
+    char    *aux;
+    ssize_t iter;
+
+    if (pos_nl == -1)
+        return (set_zero(str_sta, BUFFER_SIZE));
+    aux = (char *)malloc(ft_strlen(str_sta) + 1);
+    if (!aux)
+        return (set_zero(str_sta, BUFFER_SIZE));
+    set_zero(aux, ft_strlen(str_sta) + 1);
+    iter = -1;
+    while ((iter++) < (ft_strlen(str_sta) - pos_nl))
+        aux[iter] = str_sta[iter + pos_nl];
+    set_zero(str_sta, BUFFER_SIZE);
+	iter = -1;
+	while ((iter++) < ft_strlen(aux))
+		str_sta[iter] = aux[iter];
+	free(aux); 
+}
+
+void ft_strcat(char *str1, char *str2, ssize_t len)
+{
+    
+}
